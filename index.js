935935517fdccbe8e27337622314368f0f6da6ca
Patrick J. Wu <wotingwu@live.com>
2018-04-16 21:18:16 -0500

    rename the config settings to codeblock

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 hexo.extend.tag.register('colorblock', function(args){
-	var config = hexo.config.color_block || {};
+	var config = hexo.config.colorblock || {};
 	config.width = config.width || 10;
 	var color = parseInt(args[0]);
 	var width = parseInt(args[1]) || config.width;
